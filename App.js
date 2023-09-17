330853f3e7b2280c5f38b685312bd507e2162d7f
vanquyet00 <117863756+vanquyet00@users.noreply.github.com>
2023-09-18 03:02:29 +0700

    Revert "Login"
    This reverts commit a926a09d7954bb1cf272832d8c42db4b81830348.

--- App.js
+++ App.js
@@ -1,36 +1,14 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-import HomePage from './screen/HomePage';
-import Login from './screen/Login';
-
-const Stack = createStackNavigator();
 
 
 export default function App() {
-
+  // Hàm đăng ký
   return (
-    <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Login"
-        component={Login}
-        options={{ headerShown: false, gestureEnabled: false }}
-      />
-      <Stack.Screen
-        name="HomePage"
-        component={HomePage}
-        options={{ headerShown: false, gestureEnabled: false }}
-      />
-      {/* <Stack.Screen
-        name="Article"
-        component={Article}
-        options={{ headerShown: true, headerTitle: '' }} // Giữ thanh header, nhưng loại bỏ văn bản (tên trang) trong header
-      /> */}
-    </Stack.Navigator>
-  </NavigationContainer>
+    <View style={styles.container}>
+     
+    </View>
   );
 }
 
