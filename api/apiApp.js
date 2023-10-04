ce65339a998e82abdf31b73e9144ce2fb4a951cd
vanquyet00 <117863756+vanquyet00@users.noreply.github.com>
2023-10-04 22:38:58 +0700

    edit profile

--- api/apiApp.js
+++ api/apiApp.js
@@ -1,9 +1,9 @@
 import { Platform } from 'react-native';
 
-var baseLink 
+var baseLink;
 if (Platform.OS === 'ios') {
   // Code for iOS
-  baseLink = 'http://localhost:3002'
+  baseLink = 'http://localhost:3002';
 } else if (Platform.OS === 'android') {
   // Code for Android
   baseLink = 'http://10.0.2.2:3002';
@@ -11,11 +11,10 @@ if (Platform.OS === 'ios') {
   // Code for other platforms
 }
 
-
 var loginApiUrl = baseLink + '/dangnhap';
-var registerUrl = baseLink + '/api/themTaiKhoan'
-var getInforUrl = baseLink + '/api/getThongTinCaNhan/'
-
+var registerUrl = baseLink + '/api/themTaiKhoan';
+var getInforUrl = baseLink + '/api/getThongTinCaNhan/';
+var editProFileUrl = baseLink + '/api/suaThongTin/';
 
 const LoginApi = async ({ userName, password }) => {
   var myHeaders = new Headers();
@@ -76,26 +75,54 @@ const registerApi = async ({ name, userName, password }) => {
   }
 };
 
-
-
-  const getInfor = async ({ id}) => {
-    var requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-  
-    try {
-      const response = await fetch(getInforUrl + id, requestOptions);
-      if (!response.ok) {
-        throw new Error('Không thể kết nối đến máy chủ');
-      }
-      const result = await response.json();
-      return result.data; // Trả về kết quả cho người gọi hàm
-    } catch (error) {
-      // Xử lý lỗi nếu cần
-      throw error;
-    }
+const getInfor = async ({ id }) => {
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
   };
 
+  try {
+    const response = await fetch(getInforUrl + id, requestOptions);
+    if (!response.ok) {
+      throw new Error('Không thể kết nối đến máy chủ');
+    }
+    const result = await response.json();
+    return result.data; // Trả về kết quả cho người gọi hàm
+  } catch (error) {
+    // Xử lý lỗi nếu cần
+    throw error;
+  }
+};
 
-module.exports = { LoginApi, registerApi, getInfor};
+const editProFile = async ({ id, name, bird, sex, introduce, image }) => {
+  console.log(id + ' ' + name + ' ' + bird + ' ' + sex + ' ' + introduce + ' ' + image) 
+  var formdata = new FormData();
+  formdata.append('hoTen', name);
+  formdata.append('ngaySinh', bird);
+  formdata.append('gioiTinh', sex);
+  formdata.append('moTa', introduce);
+  formdata.append('hinhAnh', {
+    uri: image, // Đường dẫn đến hình ảnh đã chọn
+    name: 'photo.png', // Tên tùy ý cho tệp hình ảnh
+    type: 'image/png', // Loại hình ảnh
+  });
+
+  var requestOptions = {
+    method: 'POST',
+    body: formdata,
+    redirect: 'follow',
+  };
+  try {
+    const response = await fetch(editProFileUrl + id , requestOptions);
+    if (!response.ok) {
+      throw new Error('Không thể kết nối đến máy chủ');
+    }
+    const result = await response.json();
+    return result; // Trả về kết quả cho người gọi hàm
+  } catch (error) {
+    console.error('Lỗi khi gửi yêu cầu:', error); // Log lỗi nếu có
+    return null; // Trả về null nếu có lỗi
+  }
+};
+
+module.exports = { LoginApi, registerApi, getInfor, editProFile };
